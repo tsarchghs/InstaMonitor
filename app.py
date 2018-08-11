e6e04ddb386e6b6a3a0b26935d5450ae3277610e
gjergjk71 <gjergjk71@gmail.com>
2018-08-11 23:16:03 +0200

    update gets follow_back list of users

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ class userFollowData(db.Model):
     followers_length = db.Column(db.Integer())
     followings = db.Column(db.String(4294000000))
     followings_length = db.Column(db.Integer())
+    follow_back = db.Column(db.String(4294000000))
+    #unfollowed_you = db.Column(db.String(4294000000))
+    #noFollowBack = db.Column(db.String(4294000000))
     created = db.Column(db.DateTime, nullable=False,
         default=datetime.utcnow) 
     def __repr__(self):
@@ -65,25 +68,40 @@ def index():
 	if userFollowData_obj: 
 		followers = userFollowData_obj.followers_length
 		followings = userFollowData_obj.followings_length
+		follow_back = eval(userFollowData_obj.follow_back)
 	else:
 		followers = 0
 		followings = 0
 	return render_template("index.html",user_info=user_info,
 							followers=followers,
-							followings=followings)
+							followings=followings,
+							follow_back=follow_back)
 
 @app.route('/update')
 def update():
 	if not session["logged_in"] and session["api"]:
 		return redirect(url_for("login"))
 	api = session["api"]
+	last_userFollowData_obj = userFollowData.query.filter_by(username_id=api.username_id).order_by("-id").first()
 	followers = api.getTotalFollowers(api.username_id)
 	followings = api.getTotalFollowings(api.username_id)
+
+	follow_back = [] 
+	for follow in followers:
+		followBack = True
+		for following in followings:
+			if follow["pk"] == following["pk"] and follow not in follow_back:
+				followBack = False
+		if followBack:
+			follow_back.append(follow)
+
 	data = userFollowData(username_id=api.username_id,
 						  followers=str(followers),
 						  followers_length=len(followers),
 						  followings=str(followings),
-						  followings_length=len(followings))
+						  followings_length=len(followings),
+						  follow_back=str(follow_back))
+
 	db.session.add(data)
 	db.session.commit()
 	return redirect(url_for('index'))
