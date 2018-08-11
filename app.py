f68ff5dfa9cea55a06e0b057cecce1f6a2d1aea3
gjergjk71 <gjergjk71@gmail.com>
2018-08-11 18:33:18 +0200

    Logout handled differently,index passes user_info

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 SESSION_TYPE = 'filesystem'
 sess = Session(app)
 API_URL = 'https://i.instagram.com/api/v1/'
-api = InstagramAPI("forGlobal","forGlobal")
 
 @app.route('/login',methods=["GET","POST"])
 def login():
-	global api
 	if "logged_in" in session:
 		if session["logged_in"]:
 			return redirect(url_for("index"))
@@ -25,22 +23,31 @@ def login():
 		if user:
 			session["logged_in"] = True
 			session["username_id"] = api.username_id
+			session["username"] = username
+			session["password"] = password
 			return redirect(url_for("index"))
 		else:
 			return render_template("auth/login.html",invalid_credentials=True)
 
 @app.route("/logout")
 def logout():
-	global api
-	api.logout()
 	session["logged_in"] = False
 	session["username_id"] = False
+	session["username"] = False
+	session["password"] = False
 	return redirect(url_for("login"))
 
 @app.route('/index')
 def index():
-	global api
-	user_info = api.getProfileData()
+	if not "logged_in" in session:
+		return redirect(url_for("login"))
+	if "logged_in" in session:
+		if not session["logged_in"]:
+			return redirect(url_for("login"))
+	api = InstagramAPI(session["username"],session["password"])
+	api.login()
+	api.getProfileData()
+	user_info = api.LastJson
 	return render_template("index.html",user_info=user_info)
 
 if __name__ == "__main__":
